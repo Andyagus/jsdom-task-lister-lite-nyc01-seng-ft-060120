0cb2c27afeab523d40d6a5b4df62dbdbcb70780c
berellevy <berellevy@gmail.com>
2020-07-14 16:46:05 -0400

    added a delete button

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const task_button = document.querySelector('input[type="submit"]')
+    const addTaskButton = document.querySelector('input#submit-button');
+    const taskList = document.querySelector('ul#tasks');
+    const deleteListButton = document.querySelector('input#delete-list-button');
+    console.log(deleteListButton)
+
+    deleteListButton.addEventListener("click", function(e) {
+        e.preventDefault()
+        taskList.innerHTML = ""
+    })
 
 
-  task_button.addEventListener("click",function(e){
-  	e.preventDefault();
-  	console.dir(e);
-  	const task_text = document.querySelector('input[type="text"]')
-  	const textValue = task_text.value;
-  	task_text.value = ""
-		const taskList = document.querySelector('ul#tasks')
-		const newTask = document.createElement('li')
-		newTask.innerHTML = textValue
-		taskList.append(newTask);
-  });
 
-
+    addTaskButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.dir(e);
+        const task_text = document.querySelector('input[type="text"]')
+        const textValue = task_text.value;
+        task_text.value = ""
+        const newTask = document.createElement('li')
+        newTask.innerHTML = textValue
+        taskList.append(newTask);
+    })
 
 });
 
-
-
-// listen for a click on the submit button
-// get the inputted text from the submit bar 
-// clear the text field
-// create new li within the ul that has the text 
+// delete button
+// √make a button
+// √add it somewhere
+// add an event listenere
+// delete the list
